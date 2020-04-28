c8cce3ad97f31ad0d825f090520738a4b065aab1
Lindsey Gordon <53380890+lcgordon@users.noreply.github.com>
2020-04-28 12:15:18 -0400

    Update galaxies.py

--- galaxies.py
+++ galaxies.py
@@ -2,7 +2,11 @@
 """
 Created on Sun Apr 19 15:45:05 2020
 
-@author: conta
+@author: Lindsey Gordon
+
+Last Updated: 4/25/2020
+
+Loosely based on https://github.com/vpython/vpython-jupyter/blob/master/Demos/Stars.ipynb
 """
 
 #%matplotlib inline
@@ -18,31 +22,28 @@ from astropy import units as u
 from scipy import integrate
 from vpython import *
 
-#https://github.com/vpython/vpython-jupyter/blob/master/Demos/Stars.ipynb
-scene = canvas() # This is needed in Jupyter notebook and lab to make programs easily rerunnable
-scene.width = scene.height = 600
-scene.title = "Galaxy" # Display text below the 3D graphics:
-scene.caption = """Right button drag or Ctrl-drag to rotate "camera" to view scene.
-To zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.
-  On a two-button mouse, middle is left + right.
-Touch screen: pinch/extend to zoom, swipe or two-finger rotate."""
+#establishes VPython canvas
+scene = canvas() 
 
-
-#parameters
-Nstars = 200  # change this to have more or fewer stars TOTAL
+#parameters (don't play with these)
+Nstars = 400  # change this to have more or fewer stars TOTAL
 N1 = Nstars/2
 N2 = Nstars - N1
 G = 4.3e-30 #grav constant in units of pc/Msun * (pc/s)^2
 Msun = 2e30 #kg
-Rsun = 1e3 #pc (this is huge but oh well)
-L = 1e5 #pc, this is the size scale
-gal_rad = 0.5 * L #pc
-gal_mass = 10e11 #Msun
-star_mass = gal_mass/Nstars #units of Msun
-#vsun = 0.8*sqrt(G*Msun/Rsun)
 
+
+#parameters (can play with these)
+Rsun = 250 #pc (this is the size of the spheres)
+L = 3e4 #pc, this is the size scale of the disk of the milky way
+gal_rad = 0.5 * L #pc
+gal_mass = 1 * 10**11 #Msun, VISIBLE mass
+gal_mass_dark = 14 * 10**11 #dark matter mass in galaxy
+star_mass = gal_mass/Nstars #units of Msun
+
+
+#plotting purposes
 scene.range = 2*L
-#box is 2L by 2L
 scene.forward = vec(0,0,-L)
 
 xaxis = curve(color=color.gray(0.5), radius=1e3)
@@ -55,36 +56,57 @@ yaxis.append(vec(0,L,0))
 #zaxis.append(vec(0,0,0))
 #zaxis.append(vec(0,0,L))
 
+scene.width = scene.height = 600
+scene.title = "Galaxy" # Display text below the 3D graphics:
+scene.caption = """Right button drag or Ctrl-drag to rotate "camera" to view scene.
+To zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.
+  On a two-button mouse, middle is left + right.
+Touch screen: pinch/extend to zoom, swipe or two-finger rotate."""
+
+
+#Producing all the mass elements. Yes they are labelled stars. No I am not going to fix that.
+ 
 Stars = []
-star_colors = [color.cyan, color.magenta]
-
-
-#First Galaxy
+star_colors = [color.cyan, color.magenta] #each galaxy is a different color. 
 psum = vec(0,0,0)
-r = np.random.randint(0,gal_rad,Nstars)
-phi_degrees = np.random.randint(0,360,Nstars)
+
+r = np.zeros(Nstars)
+#produce density distro
+for i in range(Nstars):
+    if i <= int(0.40 * Nstars):
+        r[i] = np.random.randint(0, 0.25 * gal_rad)
+    elif int(0.40 * Nstars) < i <= int(0.70 * Nstars):
+        r[i] = np.random.randint(0.25*gal_rad, 0.5*gal_rad)
+    elif int(0.70*Nstars) < i <= int(0.9*Nstars):
+        r[i] = np.random.randint(0.5*gal_rad, 0.75*gal_rad)
+    else:
+        r[i] = np.random.randint(0.75*gal_rad, gal_rad)
+
+
+#r = np.random.randint(0,gal_rad,Nstars) #N radial distances
+
+
+phi_degrees = np.random.randint(0,360,Nstars) #N positions around axis
 phi = np.radians(phi_degrees)
-
-x = r*np.sin(phi)
-for i in range(Nstars): 
-    if i % 2 == 0:
-        x[i] = x[i] - L
-    elif i%2 == 1:
-        x[i] = x[i] + L
-        
-y = r*np.cos(phi)
-
 mom_phi = np.radians(90-phi_degrees)
 
-for i in range(Nstars):
+x = r*np.sin(phi)
+y = r*np.cos(phi)
+for i in range(Nstars): #this separates the two galaxies out
+    if i % 2 == 0:
+        x[i] = x[i] - (0.75*L)
+    elif i%2 == 1:
+        x[i] = x[i] + (0.75*L)
+        
 
-    star = sphere(pos=vector(x[i], y[i],0), make_trail=True, retain=50, trail_radius=100)
+
+for i in range(Nstars):
+    star = sphere(pos=vector(x[i], y[i],0), make_trail=True, retain=20, trail_radius=50)
     star.radius = Rsun
     star.mass = star_mass
-    star.momentum = vector(0,0,0)
-    #star.momentum = vector(mom_r*np.sin(mom_phi[0]), mom_r*np.cos(mom_phi[0]), 0) * star.mass
+    star.momentum = vector(0,0,0) #initialized as zero
     star.color = star.trail_color = star_colors[i % 2]
-    Stars.append( star )
+    Stars.append(star)
     
     
 # calculating initial velocities
@@ -100,16 +122,18 @@ for sk in range(Nstars):
     mass_internal.append(m*star_mass)
 
 v_i = np.zeros(Nstars)
+v_left = vector(6e-12, 0, 0)
+v_right = vector(-6e-12, 0,0)
 for i in range(Nstars):
-    v_i[i] = np.sqrt(G * mass_internal[i]/np.abs(r[i])) #units of km/s
+    v_i[i] = np.sqrt(G * mass_internal[i]/np.abs(r[i])) #units of pc/s
 
 
 for i in range(Nstars):
     velocity_v = vector(v_i[i]*np.sin(mom_phi[i]), -1* v_i[i]*np.cos(mom_phi[i]), 0)
-    #if i % 2 ==0:
-     #   velocity_v += vector(5e-12, 0, 0)
-    #elif i%2==1:
-     #   velocity_v += vector(-5e-12,0,0)
+    if i % 2 == 0:
+        velocity_v += v_left
+    elif i%2==1:
+        velocity_v += v_right
     Stars[i].momentum = velocity_v * Stars[i].mass
     psum = psum + Stars[i].momentum
 
@@ -141,34 +165,24 @@ def computeForces():
             r = sj.pos - pos1
             rmag2 = mag2(r)
             radius2 = sj.radius
-            if rmag2 <= (radius1 + radius2)**2:
-                r = vec(0,0,0)
+            if rmag2 <= (radius1 + radius2)**2: #so currently they ignore each other if they get too close together.
+                r = vec(0,0,0) #this is not a perfect solution but it's what i've got right now.
              
             F = F + (G*m1*sj.mass/(rmag2**1.5))*r
         si.momentum = si.momentum + F*dt
       
 
-t = 0
-v_left = vector(1e-11, 0, 0)
-v_right = vector(-1e-11, 0,0)
+t = 0 #initializes time for loop
+
 while t < 1e16:
-    #rate(100)
-    
+
     # Compute all forces on all stars
     computeForces() #REMOVE THIS COMMENT IN ORDER TO RUN SIMULATION
     
     # Having updated all momenta, now update all positions
     for i in range(len(Stars)):
         if Stars[i] is None: continue
-        
-        if i % 2 == 0:
-            Stars[i].pos = Stars[i].pos + Stars[i].momentum*(dt/star_mass) + (v_left * dt)
-        elif i% 2 == 1:
-            Stars[i].pos = Stars[i].pos + Stars[i].momentum*(dt/star_mass) + (v_right * dt)
-    #for star in Stars:
-     #   if star is None: continue
-      #  star.pos = star.pos + star.momentum*(dt/star_mass)
-            
+        Stars[i].pos = Stars[i].pos + Stars[i].momentum*(dt/star_mass) 
 
     t += dt
     print(t)
